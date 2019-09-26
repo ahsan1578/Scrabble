c5f5dd3f3f8cd67616c6eef28d4eb50687859b69
Ahsan <ahsan78@cs.unm.edu>
2019-09-26 09:50:43 -0600

    Done implementing the trie data structure. The Trie class may need more methods in future depending on requirements

---
+++ src/scrabble/Trie.java
@@ -0,0 +1,73 @@
+package scrabble;
+
+public class Trie {
+    public static final int TOTAL_LETTERS = 26;
+
+
+    class Node{
+        Node [] children = new Node[TOTAL_LETTERS];
+        boolean isWord;
+
+        Node(){
+            isWord = false;
+            for(int i =0; i<TOTAL_LETTERS; i++){
+                children[0] = null;
+            }
+        }
+
+        public void setChildren(Node node, int index){
+            children[index] = node;
+        }
+
+        public void setWord(boolean word) {
+            isWord = word;
+        }
+
+        public boolean isWord() {
+            return isWord;
+        }
+
+        public Node getChildren(int index){
+            return children[index];
+        }
+
+    }
+
+    private Node rootNode;
+    private int size;
+
+    public Trie(){
+        rootNode = new Node();
+        size = 0;
+    }
+
+    public void insert(String word){
+        int index;
+        Node parentNode = rootNode;
+
+        for(int i = 0; i<word.length(); i++){
+            index = word.charAt(i) - 'a';
+            if(parentNode.getChildren(index)==null){
+                parentNode.setChildren(new Node(), index);
+            }
+            parentNode = parentNode.getChildren(index);
+        }
+
+        parentNode.setWord(true);
+    }
+
+    public boolean isWord(String word){
+        int index;
+        Node parentNode = rootNode;
+
+        for(int i = 0; i<word.length(); i++){
+            index = word.charAt(i) - 'a';
+            if(parentNode.getChildren(index) == null){
+                return false;
+            }
+
+            parentNode = parentNode.getChildren(index);
+        }
+        return parentNode.isWord;
+    }
+}
