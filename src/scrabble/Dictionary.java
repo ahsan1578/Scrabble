338f421c8b34252bccccbaf6955340521990ea5f
Ahsan <ahsan78@cs.unm.edu>
2019-09-26 10:18:22 -0600

    Dictionary class done until the point where it can be checked if a word exists

--- src/scrabble/Dictionary.java
+++ src/scrabble/Dictionary.java
@@ -1,4 +1,33 @@
 package scrabble;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Dictionary {
+    private Trie trie;
+
+    public Dictionary(String dictionaryPathName){
+        trie = new Trie();
+        InputStream inputStream = getClass().getResourceAsStream(dictionaryPathName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = null;
+        try {
+            while ((line=reader.readLine()) !=null){
+                trie.insert(line);
+            }
+        }catch (Exception e){
+            System.out.println("Wrong dictionary file");
+        }
+    }
+
+    public boolean doesWordExist(String word){
+        return trie.isWord(word);
+    }
+
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary("sowpods.txt");
+        System.out.println(dictionary.doesWordExist("overfl"));
+        System.out.println(dictionary.doesWordExist("overflow"));
+    }
 }
